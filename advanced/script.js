cfcc90636cc1486db312f0bd8ba38fc5235bd572
d.odincov <d.odincov@pfsz.ru>
2025-06-16 14:08:58 +0700

    Добавил поиск, добавил форму в для заказа

--- advanced/script.js
+++ advanced/script.js
@@ -27,6 +27,44 @@ function renderGoodsList(list) {
     goodsList.innerHTML = html;
 }
 
+// Поиск товаров по регулярному выражению
+function searchGoods() {
+    // Значение из поля поиска
+    const searchInput = document.getElementById('search-area').value.trim();
+
+    // Если поле пустое - показывает все товары
+    if (searchInput === '') {
+        renderGoodsList(goods);
+        return;
+    }
+
+    try {
+        // - 'i' - регистронезависимый поиск
+        // - игнорирует специальные символы, кроме указанных в шаблоне
+        const regex = new RegExp(searchInput, 'i');
+
+        // Фильтрация товаров
+        const filteredGoods = [];
+        for (let i = 0; i < goods.length; i++) {
+            // Поиск совпадений в названии товара
+            if (regex.test(goods[i].product_name)) {
+                filteredGoods.push(goods[i]);
+            }
+        }
+        //Вывод "Товары не найдены" если список товаров пуст
+        if (filteredGoods.length === 0) {
+            document.querySelector('.goods-list').innerHTML = `<p>Товары по запросу "${searchInput}" не найдены</p>`;
+            return;
+        }
+
+        // Отображение результатов
+        renderGoodsList(filteredGoods);
+
+    } catch (e) {
+
+    }
+}
+
 // Ддобавление товара в корзину
 function addToCart(productId) {
     // Находим товар по ID
@@ -103,6 +141,21 @@ function closeCart() {
     document.getElementById('cartModal').style.display = 'none';
 }
 
+function openCartOrder() {
+    if (cartItems == "") {
+        showNotificationBad(`Ваша корзина пуста!`)
+    } else {
+        document.getElementById('cartModalOrder').style.display = 'block';
+        document.getElementById('cartModal').style.display = 'none';
+    }
+}
+
+// Закрытие модального окна корзины
+function closeCartOrder() {
+    document.getElementById('cartModalOrder').style.display = 'none';
+    document.getElementById('cartModal').style.display = 'block';
+}
+
 // Оформление заказа
 function checkout() {
     // Общая сумма
@@ -115,6 +168,7 @@ function checkout() {
     // Обновление интерфейса
     updateCartUI();
     // Закрытие модального окна
+    closeCartOrder();
     closeCart();
     // Сообщение о успешном оформлении
     showNotification(`Заказ успешно оформлен! Сумма: ${total}₽`)
@@ -156,5 +210,72 @@ function showNotification(text) {
     // Автозакрытие через 5 секунд
     setTimeout(() => notification.onclick(), 5000);
 }
+
+function showNotificationBad(text) {
+    // Настройка уведомления
+    const notificationBad = document.createElement('div');
+    notificationBad.className = 'notification-bad';
+    notificationBad.innerHTML = text + '<div class="progress-bar-bad"></div>';
+    document.body.appendChild(notificationBad);
+
+    // Задержка анимации
+    setTimeout(() => {
+        notificationBad.classList.add('show-bad');
+        notificationBad.querySelector('.progress-bar-bad').style.transform = 'scaleX(0)';
+    }, 10);
+
+    // Закрытие уведомления
+    notificationBad.onclick = () => {
+        notificationBad.classList.remove('show-bad');
+        setTimeout(() => notificationBad.remove(), 300);
+    };
+
+    // Автозакрытие через 5 секунд
+    setTimeout(() => notificationBad.onclick(), 5000);
+}
+
+
+//Сброс красной обводки текстовых полей при ошибке
+function removeError(inputElement) {
+    inputElement.classList.remove('error');
+}
+
+//Валидация формы
+function validateForm() {
+    let isValid = true;
+
+    // Валидация Имени
+    const name = document.getElementById('nameArea').value;
+    if (!/^[a-zA-Zа-яА-ЯёЁ\s]+$/.test(name)) {
+        document.getElementById('nameArea').classList.add('error');
+        isValid = false;
+        showNotificationBad(`Имя должно содержать только буквы!`)
+        return false;
+    }
+
+    // Валидация Телефона
+    const phone = document.getElementById('numberArea').value;
+    if (!/^\+7\(\d{3}\)\d{3}-\d{4}$/.test(phone)) {
+        document.getElementById('numberArea').classList.add('error');
+        isValid = false;
+        showNotificationBad(`Телефон должен иметь вид +7(000)000-0000!`)
+        return false;
+    }
+
+    // Валидация E-mail
+    const email = document.getElementById('emailArea').value;
+    if (!/^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$/.test(email)) {
+        document.getElementById('emailArea').classList.add('error');
+        isValid = false;
+        showNotificationBad(`E-mail должен иметь вид example@example.ru!`)
+        return false;
+    }
+
+    if (isValid) {
+        checkout()
+        return true;
+    }
+}
+
 // Загрузка товаров при загрузке страницы
 loadGoods();
